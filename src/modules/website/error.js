87d55b4c61b3dae0b26fa6a2f09ca984287f232a
Eliiia <grzesiul1995@gmail.com>
2022-01-03 17:47:05 +0000

    Fixed resulkt in src/modules/website/main.js

--- src/modules/website/error.js
+++ src/modules/website/error.js
@@ -4,10 +4,12 @@ function getPage(err) {
     return fs.readFileSync(`${__dirname}/www/err/${err}.html`)
 }
 
-module.exports = (err, res) => {
+module.exports = (err) => {
     switch(err) {
         case 404: // Not Found
-            res.writeHead(404)
-            res.end(getPage(404)) // get 404 file
+            return {
+                status: 404,
+                body: getPage(404)
+            }
     }
 }
--- src/modules/website/main.js
+++ src/modules/website/main.js
@@ -4,21 +4,24 @@ const error = require("./error.js")
 const log = require("../../log.js")
 
 module.exports = (req, res) => {
+    let result
 
     if(req.method == "GET") {
         if(req.url.endsWith("/")) req.url += "index.html"
         if(req.url.endsWith(".html")) res.setHeader("Content-Type", "text/html")
 
-        fs.readFile(`${__dirname}/www${req.url}`, 
-            (err, data) => {
-                if(err) error(404, res)
-                else {
-                    res.writeHead(200)
-                    res.end(data)
-                }
+        try {
+            result = {
+                status: 200,
+                body: fs.readFileSync(`${__dirname}/www${req.url}`)
             }
-        )
+        } catch(e) {
+            result = error(404)
+        }
     }
+
+    res.writeHead(result.status)
+    res.end(result.body)
 
     log("website", `${req.socket.remoteAddress} ${req.method} ${req.url}`, result.status)
 }
