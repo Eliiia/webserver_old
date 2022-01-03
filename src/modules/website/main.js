69aa92a15521f8f32980dbfe4eb8e127ae784d4f
Eliiia <grzesiul1995@gmail.com>
2022-01-03 01:27:34 +0000

    Move log file to src/

--- src/modules/website/main.js
+++ src/modules/website/main.js
@@ -1,12 +1,13 @@
 const fs = require("fs")
 
 const error = require("./error.js")
-const log = require("../log.js")
+const log = require("../../log.js")
 
 module.exports = (req, res) => {
 
     if(req.method == "GET") {
         if(req.url.endsWith("/")) req.url += "index.html"
+        if(req.url.endsWith(".html")) res.setHeader("Content-Type", "text/html")
 
         fs.readFile(`${__dirname}/www${req.url}`, 
             (err, data) => {
