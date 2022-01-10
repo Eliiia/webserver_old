9403b46420815425a97166067d81bae86c0dd946
Eliiia <grzesiul1995@gmail.com>
2022-01-10 22:35:09 +0000

    Add colours & change date format in `log.js`

--- src/log.js
+++ src/log.js
@@ -3,11 +3,18 @@ const fs = require("fs")
 module.exports = (moduleName, text, status) => {
     let d = new Date()
 
-    let logText = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] [${moduleName}] ${text} => ${status}`
-    let logText2 = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] ${text} => ${status}`
+    let colour = ""
+    if(moduleName == "website" || moduleName == "api") {
+        if(100 <= status && status < 400) colour = "[32m"
+        else if(400 <= status && status < 500) colour = "[33m"
+        else if(500 <= status && status < 600) colour = "[31m"
+    } else throw `log.js; unknown moduleName passed\n\tmoduleName: ${moduleName}`
+
+    let logText = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] [${moduleName}] ${text} => \x1b${colour}${status}\x1b[39m`
+    let logTextMod = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] ${text} => \x1b${colour}${status}\x1b[39m`
 
     console.log(logText)
     
-    fs.appendFileSync(`../logs/all_${d.getDate()}-${d.getMonth()}-${d.getFullYear()}.txt`, logText+"\n")
-    fs.appendFileSync(`../logs/${moduleName}_${d.getDate()}-${d.getMonth()}-${d.getFullYear()}.txt`, logText2+"\n")
+    fs.appendFileSync(`../logs/all_${d.getFullYear()}-${d.getMonth()}-${d.getDate()}.txt`, logText+"\n")
+    fs.appendFileSync(`../logs/${moduleName}_${d.getFullYear()}-${d.getMonth()}-${d.getDate()}.txt`, logTextMod+"\n")
 }
