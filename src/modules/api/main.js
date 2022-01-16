016d01d10753a7fc437e84fa5bfb412aea4dc5ed
Eliiia <grzesiul1995@gmail.com>
2022-01-16 19:13:17 +0000

    require application/json or no content-type

--- src/modules/api/main.js
+++ src/modules/api/main.js
@@ -2,6 +2,8 @@ const log = require("../../log.js")
 
 const routes = {
     "ping": [ ["GET"], require("./routes/ping.js") ],
+    "login": [ ["POST"], require("./routes/login.js") ],
+    "register": [ ["POST"], require("./routes/register.js") ],
 }
 
 module.exports = (req, res) => {
@@ -13,8 +15,9 @@ module.exports = (req, res) => {
 
     if(!routes[args[1]] || args.length == 2) result = { status: 404, body: "404 Not Found" }
     else if(!routes[args[1]][0].includes(req.method)) result = { status: 405, body: "405 Method Not Allowed" }
+    else if(!["application/json", undefined].includes(req.headers["content-type"])) result = { status: 400, body: `400 Bad Request\n\n`}
 
-    else {
+    if(result == undefined) {
         try { result = routes[args[1]][1](req, res, args) }
         catch(e) { result = {status: 500, body: `500 Internal Server Error\n\n${e}`} }
     }
