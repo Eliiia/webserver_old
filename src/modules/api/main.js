706c105fe3368c6e94b4fa452ef05c067ab03f8b
Eliiia <grzesiul1995@gmail.com>
2022-01-16 11:33:26 +0000

    Re-made method types in api/main.js

--- src/modules/api/main.js
+++ src/modules/api/main.js
@@ -1,27 +1,23 @@
 const log = require("../../log.js")
 
 const routes = {
-    "ping": require("./routes/ping.js")
+    "ping": [ ["GET"], require("./routes/ping.js") ],
 }
 
 module.exports = (req, res) => {
     let result
 
-    if(req.method == "GET") {
-        res.setHeader("Content-Type", "application/json")
+    res.setHeader("Content-Type", "application/json")
 
-        const args = req.url.split("/")
+    const args = req.url.split("/")
 
-        if(!routes[args[1]]) result = { status: 404, body: "404 Not Found" }
+    if(!routes[args[1]] || args.length == 2) result = { status: 404, body: "404 Not Found" }
+    else if(!routes[args[1]][0].includes(req.method)) result = { status: 405, body: "405 Method Not Allowed" }
 
-        if(args.length == 2) result = { status: 404, body: "404 Not Found" }
-        if(result == undefined && routes[args[1]]) {
-            try { result = routes[args[1]](req, res, args) }
-        
-            catch(e) { result = {status: 500, body: `500 Internal Server Error\n\n${e}`} }
-        }
+    else {
+        try { result = routes[args[1]][1](req, res, args) }
+        catch(e) { result = {status: 500, body: `500 Internal Server Error\n\n${e}`} }
     }
-    else result = { status: 405, body: "405 Method Not Allowed" }
 
     res.statusCode = result.status
     res.end(result.body)
