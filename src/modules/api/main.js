fb6431b5393d8e87b62cb2202df51ceaa86b40ed
Eliiia <grzesiul1995@gmail.com>
2022-02-06 00:08:15 +0000

    API: Send objects on error
    ...so that things using it dont instantly crash when trying to parse a little string as JSON, and can instead check whether an "error" variable exists before proceeding

--- src/modules/api/main.js
+++ src/modules/api/main.js
@@ -2,6 +2,7 @@ const log = require("../../log.js")
 
 const routes = {
     "ping": [ ["GET"], require("./routes/ping.js") ],
+    "register": [ ["POST"], require("./routes/register.js")],
 }
 
 module.exports = (req, res) => {
@@ -11,13 +12,13 @@ module.exports = (req, res) => {
 
     const args = req.url.split("/")
 
-    if(!routes[args[1]] || args.length == 2) result = { status: 404, body: "404 Not Found" }
-    else if(!routes[args[1]][0].includes(req.method)) result = { status: 405, body: "405 Method Not Allowed" }
-    else if(!["application/json", undefined].includes(req.headers["content-type"])) result = { status: 400, body: `400 Bad Request\n\n`}
+    if(!routes[args[1]] || args.length == 2) result = { status: 404, body: { error: "404 Not Found" } }
+    else if(!routes[args[1]][0].includes(req.method)) result = { status: 405, body: { error: "405 Method Not Allowed" } }
+    else if(!["application/json", undefined].includes(req.headers["content-type"])) result = { status: 400, body: { error: `400 Bad Request\n\n` } }
 
     if(result == undefined) {
         try { result = routes[args[1]][1](req, res, args) }
-        catch(e) { result = {status: 500, body: `500 Internal Server Error\n\n${e.stack}`} }
+        catch(e) { result = { status: 500, body: { error: `500 Internal Server Error\n\n${e.stack}` } } }
     }
 
     res.statusCode = result.status
