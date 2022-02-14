39a0493df8d59ad5f71428663f9e6be28db9fde2
Eliiia <grzesiul1995@gmail.com>
2022-02-14 23:06:11 +0000

    Make lack of trailing slashes redirect

--- src/modules/website/main.js
+++ src/modules/website/main.js
@@ -4,12 +4,21 @@ const error = require("./error.js")
 const mime = require("./mime.js")
 const log = require("../../log.js")
 
+const conf = require("../../config.json").web
+
 module.exports = (req, res) => {
     let result
 
     if(req.method == "GET") {
         if(req.url.endsWith("/")) req.url += "index.html"
-        if(fs.existsSync(`${__dirname}/www${req.url}`)) if(fs.lstatSync(`${__dirname}/www${req.url}`).isDirectory()) req.url += "/index.html"
+        if(fs.existsSync(`${__dirname}/www${req.url}`)) {
+            if(fs.lstatSync(`${__dirname}/www${req.url}`).isDirectory()) {
+                if(fs.existsSync(`${__dirname}/www${req.url}/index.html`)) {
+                    res.writeHead(308, {Location: `https://${conf.domain}${req.url}/`}).end()
+                    log("website", `${req.socket.remoteAddress} ${req.method} ${req.url}`, 308)
+                    return
+                }}}
+        
         contentType = mime(req.url)
 
         try {
