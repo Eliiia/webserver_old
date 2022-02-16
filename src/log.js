c4f639f23281d41e9df377b539a4a9e5df1c5bac
Eliiia <grzesiul1995@gmail.com>
2022-02-16 17:31:06 +0000

    throw actual error object in `log.js`

--- src/log.js
+++ src/log.js
@@ -8,7 +8,7 @@ module.exports = (moduleName, text, status) => {
         if(100 <= status && status < 400) colour = "[32m"
         else if(400 <= status && status < 500) colour = "[33m"
         else if(500 <= status && status < 600) colour = "[31m"
-    } 
+    }
     else if(moduleName == "discord") {
         if(status) colour = "[32m"
         else colour = "[31m"
@@ -19,7 +19,7 @@ module.exports = (moduleName, text, status) => {
     else if(moduleName == "redirect") {
         colour = "[32m"
     }
-    else throw `log.js; unknown moduleName passed\n\tmoduleName: ${moduleName}`
+    else throw Error(`log.js; unknown moduleName passed\n\tmoduleName: ${moduleName}`)
 
     let logText = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] [${moduleName}] ${text} => \x1b${colour}${status}\x1b[39m`
     let logTextMod = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}] ${text} => \x1b${colour}${status}\x1b[39m`
